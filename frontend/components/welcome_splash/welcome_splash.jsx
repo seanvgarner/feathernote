2f69124f444fe639f2f18d53a46b4b8f2cd51ccc
seanvgarner <seanvgarner@gmail.com>
2016-12-06 19:44:13 -0500

    Place logo on splash page

--- frontend/components/welcome_splash/welcome_splash.jsx
+++ frontend/components/welcome_splash/welcome_splash.jsx
@@ -5,11 +5,18 @@ import { Link } from 'react-router';
 // redesign this presentational component to fit welcome spash page/ logget out view
 const WelcomeSplash = ({ children }) => {
     return (
-      <div className="content-container">
-        <nav className="sign-in-navbar">
-          <h1>FEATHERNOTE</h1>
-            <Link to='/signin'>Sign In</Link>
-        </nav>
+      <div className="content-container group">
+        <header className="welcome-header group">
+          <div className="logo-container group">
+            <div className="img-logo-wrapper">
+              <img src={ window.featherLogo }></img>
+            </div>
+            <h1>FEATHERNOTE</h1>
+          </div>
+          <nav className="sign-in-navbar">
+              <Link to='/signin'>Sign In</Link>
+          </nav>
+        </header>
         { children ||
           <div content="jumbotron-container">
             <Link to='/signup' className="sign-up-btn">Sign Up</Link>
